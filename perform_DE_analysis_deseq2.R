dee3724a291c8dea08be3f331ccaffbbb11536dd
Sagnik Banerjee <sagnikbanerjee15@gmail.com>
2021-11-21 11:01:07 -0600

    Update perform_DE_analysis_deseq2.R

--- perform_DE_analysis_deseq2.R
+++ perform_DE_analysis_deseq2.R
@@ -31,12 +31,12 @@ setwd(out_dir)
 counts_file<-args[9]
 pval_cutoff<-as.double(args[10])
 print(args[11:length(args)])
-num_of_replicates<-length(args[12:length(args)])/2
+num_of_replicates<-length(args[11:length(args)])/2
 
 raw_count <- read.delim(counts_file, header=FALSE, row.names=1)
 countdata <- round(raw_count)
 colSums(countdata)/gm_mean(colSums(countdata))
-colnames(countdata)<-args[12:length(args)]
+colnames(countdata)<-args[11:length(args)]
 
 # Remove all gene which has 0 value in all sample
 all <- apply(countdata, 1, function(x) all(x==0) )
